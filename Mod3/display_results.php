3527b5763cba0453a87e996bf35976ec85e1d656
bahayzl <bahayzl@pointpark.edu>
2022-10-11 17:51:31 -0400

    Finished the module lab

--- Mod3/display_results.php
+++ Mod3/display_results.php
@@ -1,5 +1,27 @@
 <?php
 
+    //Capture data from original screen
+    $investment = filter_input(INPUT_POST, 'investment', FILTER_VALIDATE_FLOAT);
+    $interest_rate = filter_input(INPUT_POST, 'interest_rate', FILTER_VALIDATE_FLOAT);
+    $years = filter_input(INPUT_POST, 'years', FILTER_VALIDATE_FLOAT);
+
+    //validation
+    if ($investment === FALSE) {
+        $error_message = 'Investment must be a valid number.';
+    } else if ($investment <=0) {
+        $error_message = 'Investment must be greater than zero.';
+    } else {
+        $error_message = '';
+    }
+
+    //if error message
+    if($error_message != '') {
+        include('index.php');
+        exit();
+    }
+
+    $investment_f = '$'.number_format($investment, 2);
+
 ?>
 
 <!DOCTYPE html>
---
+++ Mod3/notfound.php
@@ -0,0 +1,10 @@
+<?php
+
+?>
+
+<html>
+    <title>Not Found</title>
+    <head></head>
+    <h1>Conducter, we have a problem!</h1>
+    <body></body>
+</html>
