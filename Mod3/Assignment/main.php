1659894e379e9d247b0ebe69f30d22a926b20327
bahayzl <bahayzl@pointpark.edu>
2022-10-23 20:24:16 -0400

    Module 3 assignment
    Finished mod 3 work

--- Mod3/Assignment/main.php
+++ Mod3/Assignment/main.php
@@ -2,39 +2,55 @@
   // set the default values
   if (!isset($apple_variant)) { $apple_variant = ''; }
   if (!isset($list_price)) { $list_price = ''; }
-  if (!isset($discount_percent)) { $discount_percent = ''; }
+  if (!isset($number_of_apples)) { $number_of_apples = ''; }
+  if (!isset($employee_discount)) { $employee_discount = ''; }
 ?>
 <html>
     <head>
-        <title>Apple Discounts</title>
+        <title>Apple Order</title>
         <link rel="stylesheet" type="text/css" href="main.css">
     </head>
 <body>
     <main>
-        <h1>Apple Discount</h1>
+        <h1>Apple Order</h1>
         <?php if (!empty($error_message)) { ?>
             <p class="error"><?php echo $error_message; ?></p>
         <?php } //end if statement ?>
 
-        <form action="applediscount.php" method="post">
+        <form action="order_confirmation.php" method="post">
             <div id="data">
-                <!-- Product Description -->
+                <!-- Apple Variant -->
                 <label>Apple Variant:</label>
                 <input type="text"  name="apple_variant"
                     value="<?php echo htmlspecialchars($apple_variant); ?>"><br>
                 <!-- List Price -->
                 <label>List Price:</label>
-                <input type="text"  name="list_price"
-                    value="<?php echo htmlspecialchars($list_price); ?>"><br>
-                <!-- Discount Percentage -->
-                <label>Discount Percentage:</label>
-                <input type="text"  name="discount_percent"
-                    value="<?php echo htmlspecialchars($discount_percent); ?>"><br>
-                <span>%</span><br>
+                <?php if (!empty($error_message)) { ?>
+            <p> class="error"><?php echo $error_message; ?></p>
+        <?php } //end if ?>
+        <form action="order_confirmation.php" method="post">
+            <div id="data">
+                <select name="list_price">
+                    <?php for ($v = 0.50; $v <=4.00; $v += 0.50) : ?>
+                        <option value="<?php echo $v; ?>" >
+                            <?php echo $v; ?>
+                        </option>
+                    <?php endfor; ?>
+                </select>
             </div>
+                <!-- # of Apples -->
+                <label># of Apples:</label>
+                <input type="text"  name="number_of_apples"
+                value="<?php echo htmlspecialchars($number_of_apples); ?>"><br>
+                <!-- Are you an employee? -->
+                <div id ="buttons">
+                <label>&nbsp;</label>
+                <input type="checkbox" name="employee_status"
+                    value="<?php echo $employee_status;?>">Employee?
+                <br>
+                <label>&nbsp;</label><br>
 
             <div id="buttons">
-                <label>&nbsp;</label>
                 <input type="submit" value="Calculate"><br>
         </form>
      </main>
--- Mod3/Assignment/order_confirmation.php
+++ Mod3/Assignment/order_confirmation.php
@@ -2,7 +2,9 @@
     // capture the data from the form
     $apple_variant = filter_input(INPUT_POST, 'apple_variant');
     $list_price = filter_input(INPUT_POST, 'list_price', FILTER_VALIDATE_FLOAT);
+    $number_of_apples = filter_input(INPUT_POST, 'number_of_apples', FILTER_VALIDATE_INT);
     $discount_percent = filter_input(INPUT_POST, 'discount_percent', FILTER_VALIDATE_INT);
+    $employee_status = filter_input(INPUT_POST, 'employee_status', FILTER_VALIDATE_FLOAT);
 
 //validate the date
 $error_message = ''; //baseline value
@@ -17,12 +19,18 @@ if ($list_price === FALSE) {
     $error_message .= 'List price must be greater than 0.<br>';
 }
 
+// validate the number of apples ordered
+if ($number_of_apples === FALSE) {
+    $error_message .= '# of apples must be a valid number.<br>';
+} else if ($list_price <= 0) {
+    $error_message .= '# of apples must be greater than 0.<br>';
+}
 
-// validate discount percent
-if ($discount_percent === FALSE) {
-    $error_message .= 'Discount Percent must be a valid whole number.<br>';
-} else if ($discount_percent <= 0) {
-    $error_message .= 'Discount Percent must be greater than 0.<br>';
+// validate employee status
+if ($employee_status === TRUE) {
+    $employee_discount_percent = .20;
+} else if ($employee_status <= 0) {
+    $employee_discount_percent = 0;
 }
 
 if ($error_message != '') {
@@ -31,7 +39,7 @@ if ($error_message != '') {
 }
 
     //Calculate discount
-    $discount = $list_price * $discount_percent * .01;
+    $discount = $list_price * $employee_discount_percent * .01;
     $discount_price = $list_price - $discount;
 
     //State Tax
@@ -43,7 +51,7 @@ if ($error_message != '') {
     $local_tax = $discount_price * LOCAL_TAX_PERCENT * .01;
 
     // Identify sales total
-    $sales_total = $discount_price + $state_tax + $local_tax;
+    $sales_total = ($discount_price + $state_tax + $local_tax) * $number_of_apples;
 
     // formatting...
     $list_price_formatted = "$".number_format($list_price, 2);
@@ -59,10 +67,13 @@ if ($error_message != '') {
 ?>
 <html>
     <head>
-        <title>Apple Discounts</title>
+        <title>Order Confirmation</title>
         <link rel="stylesheet" type="text/css" href="main.css">
     </head>
     <body>
+    <form action="main.php" method="POST">
+    <input type="submit" value="Return"/>
+    </form>
         <main>
             <h1>Apple Discount</h1>
 
@@ -72,8 +83,8 @@ if ($error_message != '') {
             <label>List Price:</label>
             <span><?php echo htmlspecialchars($list_price_formatted); ?></span><br>
 
-            <label>Standard Discount:</label>
-            <span><?php echo htmlspecialchars($discount_percent_formatted); ?></span><br>
+            <label># of Apples Ordered:</label>
+            <span><?php echo htmlspecialchars($number_of_apples); ?></span><br>
 
             <label>Discount Amount:</label>
             <span><?php echo $discount_formatted; ?></span><br>
